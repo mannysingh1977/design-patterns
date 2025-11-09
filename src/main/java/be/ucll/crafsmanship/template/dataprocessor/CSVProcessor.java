42fea343a43d69ae39f07169fcdfdf9e65a7d770
Manny <mannysinghb2005@gmail.com>
2025-11-09 23:47:40 +0100

    Extend CSVProcessor to inherit from DataProcessor and implement load, transform, and save methods for CSV data handling

--- src/main/java/be/ucll/crafsmanship/template/dataprocessor/CSVProcessor.java
+++ src/main/java/be/ucll/crafsmanship/template/dataprocessor/CSVProcessor.java
@@ -3,28 +3,34 @@ package be.ucll.crafsmanship.template.dataprocessor;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CSVProcessor {
+public class CSVProcessor extends DataProcessor {
     private List<String> data;
 
     public CSVProcessor(String inputFile, String outputFile) {
-        //super(inputFile, outputFile);
+        super(inputFile, outputFile);
         this.data = new ArrayList<>();
     }
 
+    @Override
     protected void load() {
         System.out.println("loading CSV...");
-        this.data.add("line 1");
-        this.data.add("line 2");
-        this.data.add("line 3");
+        data.add("name,age,city");
+        data.add("john,25,new york");
+        data.add("jane,30,london");
+        data.add("bob,35,paris");
     }
 
+    @Override
     protected void transform() {
         System.out.println("transforming CSV...");
         data.replaceAll(String::toUpperCase);
     }
 
+    @Override
     protected void save() {
         System.out.println("saving CSV...");
+        for (String line : data) {
+            System.out.println("  -> " + line);
+        }
     }
-
 }
