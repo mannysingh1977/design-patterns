79e9ead0caeea906b5f6b73e8ffca143b07fde03
Manny <mannysinghb2005@gmail.com>
2025-11-09 23:30:24 +0100

    Refactor Client class to enable button command functionality in PS5Controller

--- src/main/java/be/ucll/crafsmanship/command/gamecontroller/Client.java
+++ src/main/java/be/ucll/crafsmanship/command/gamecontroller/Client.java
@@ -5,20 +5,28 @@ public class Client {
         Receiver receiver = new Receiver();
 
         Command crossCommand = new CrossButtonCommand(receiver);
-//        Command triangleCommand = new TriangleButtonCommand(receiver);
-//        Command squareCommand = new SquareButtonCommand(receiver);
-//        Command circleCommand = new CircleButtonCommand(receiver);
-//
-//        PS5Controller controller = new PS5Controller();
-//        controller.setCrossButton(crossCommand);
-//        controller.setTriangleButton(triangleCommand);
-//        controller.setSquareButton(squareCommand);
-//        controller.setCircleButton(circleCommand);
-//
-//        // Simulate button presses
-//        controller.pressCross();
-//        controller.pressTriangle();
-//        controller.pressSquare();
-//        controller.pressCircle();
+        Command triangleCommand = new TriangleButtonCommand(receiver);
+        Command squareCommand = new SquareButtonCommand(receiver);
+        Command circleCommand = new CircleButtonCommand(receiver);
+
+        PS5Controller controller = new PS5Controller();
+        controller.setCrossButton(crossCommand);
+        controller.setTriangleButton(triangleCommand);
+        controller.setSquareButton(squareCommand);
+        controller.setCircleButton(circleCommand);
+
+        controller.pressCross();
+        controller.pressTriangle();
+        controller.pressSquare();
+        controller.pressCircle();
+
+        controller.setCrossButton(circleCommand);
+        controller.setCircleButton(crossCommand);
+
+        controller.pressCross();
+        controller.pressCircle();
+
+        controller.setTriangleButton(squareCommand);
+        controller.pressTriangle();
     }
 }
