1752a41e9c6a5bf944684263a6b4899f1674bab7
Manny <mannysinghb2005@gmail.com>
2025-11-09 23:18:51 +0100

    Add button command methods to PS5Controller class

--- src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
+++ src/main/java/be/ucll/crafsmanship/command/gamecontroller/PS5Controller.java
@@ -1,5 +1,48 @@
 package be.ucll.crafsmanship.command.gamecontroller;
 
 public class PS5Controller {
-    // todo
+    private Command crossButton;
+    private Command triangleButton;
+    private Command squareButton;
+    private Command circleButton;
+
+    public void setCrossButton(Command command) {
+        this.crossButton = command;
+    }
+
+    public void setTriangleButton(Command command) {
+        this.triangleButton = command;
+    }
+
+    public void setSquareButton(Command command) {
+        this.squareButton = command;
+    }
+
+    public void setCircleButton(Command command) {
+        this.circleButton = command;
+    }
+
+    public void pressCross() {
+        if (crossButton != null) {
+            crossButton.execute();
+        }
+    }
+
+    public void pressTriangle() {
+        if (triangleButton != null) {
+            triangleButton.execute();
+        }
+    }
+
+    public void pressSquare() {
+        if (squareButton != null) {
+            squareButton.execute();
+        }
+    }
+
+    public void pressCircle() {
+        if (circleButton != null) {
+            circleButton.execute();
+        }
+    }
 }
