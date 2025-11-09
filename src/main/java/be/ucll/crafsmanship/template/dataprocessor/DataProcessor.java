88b60a87de608629135c5285938da9976e0573dc
Manny <mannysinghb2005@gmail.com>
2025-11-09 23:42:23 +0100

    Refactor DataProcessor class to be abstract and add structure for file processing methods

--- src/main/java/be/ucll/crafsmanship/template/dataprocessor/DataProcessor.java
+++ src/main/java/be/ucll/crafsmanship/template/dataprocessor/DataProcessor.java
@@ -1,9 +1,31 @@
 package be.ucll.crafsmanship.template.dataprocessor;
 
-public class DataProcessor {
+public abstract class DataProcessor {
+    protected String inputFile;
+    protected String outputFile;
+
+    public DataProcessor(String inputFile, String outputFile) {
+        this.inputFile = inputFile;
+        this.outputFile = outputFile;
+    }
+
     public final void process() {
-//        load();
-//        transform();
-//        save();
+        load();
+        transform();
+        if (shouldValidate()) {
+            validate();
+        }
+        save();
+    }
+
+    protected abstract void load();
+    protected abstract void transform();
+    protected abstract void save();
+
+    protected boolean shouldValidate() {
+        return false;
+    }
+
+    protected void validate() {
     }
 }
